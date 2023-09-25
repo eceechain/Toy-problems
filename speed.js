9ea75b5a9b5a67cff23901450f1ff16c114167a9
ecee <you@example.com>
2023-09-25 13:19:22 +0300

    Done

--- speed.js
+++ speed.js
@@ -8,15 +8,15 @@ function demeritPoints(speed) {
     if (speed <= speedLimit) {
         console.log("OK");
     } else {
-       // demerit points based on speedlimit.
+        // demerit points based on speedlimit.
         const demeritPoints = Math.floor((speed - speedLimit) / 5);
-        
+
         //suspension demerit points.
         if (demeritPoints >= 12) {
-            console.log("License suspended"); 
+            console.log("License suspended");
             // calculating Demerit points.
         } else {
-            console.log(`Points: ${demeritPoints}`); 
+            console.log(`Points: ${demeritPoints}`);
         }
     }
 }
